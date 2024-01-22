57255e05ae59c335ff6284d07291c360762e3963
Kevin Mueller <kevin.mueller@tuta.com>
2024-01-22 14:23:11 +0100

    added reload function for more pkmn in a button and improved the loading time

--- render.js
+++ render.js
@@ -1,19 +1,18 @@
-function renderPokemonTiles(data) {
-for (let i = 0; i < data.length; i++) {
-    const tile = data[i];
+function renderPokemonTiles(data, dataOffs) {
+    for (let i = 0 + dataOffs ; i < data.length; i++) {
+        const tile = data[i];
 
-    document.getElementById('tiles-container').innerHTML+=templateTile(i);
-    document.getElementById(`tile${i}`).style.background = `var(--${tile.types[0]})`;
-    document.getElementById(`pokemon-name${i}`).innerHTML = firstLetterToCapital(tile.name);
-    for (let j = 0; j < tile.types.length; j++) {
-        const element = tile.types[j];
-        console.log(element)
-        document.getElementById(`tile-info${i}`).innerHTML += templateType(element);
+        document.getElementById('tiles-container').innerHTML += templateTile(i);
+        document.getElementById(`tile${i}`).style.background = `var(--${tile.types[0]})`;
+        document.getElementById(`pokemon-name${i}`).innerHTML = firstLetterToCapital(tile.name);
+        for (let j = 0; j < tile.types.length; j++) {
+            const element = tile.types[j];
+            console.log(element)
+            document.getElementById(`tile-info${i}`).innerHTML += templateType(element);
+        }
+        document.getElementById(`sprite${i}`).src = tile.sprite
+        document.getElementById(`sprite${i}`).alt = tile.name
     }
-    document.getElementById(`sprite${i}`).src = tile.sprite
-    document.getElementById(`sprite${i}`).alt = tile.name
-}
-
-
+    dataOffset += 25;
 }
 
--- script.js
+++ script.js
@@ -1,33 +1,39 @@
-let pokemonData =[]
+let pokemonData = []
+let dataOffset = 0;
 let currentPokemon;
 
-async function init(){
+async function init() {
     await loadPokemon();
-    renderPokemonTiles(pokemonData);
+    renderPokemonTiles(pokemonData,dataOffset);
 }
 
 async function loadPokemon() {
-    for (let i = 1; i <= 25; i++) {
+    for (let i = 1 + dataOffset; i <= 25 + dataOffset; i++) {
         let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
         let response = await fetch(url);
         let responseAsJson = await response.json();
         let name = responseAsJson['name']
         let sprite = responseAsJson['sprites']['other']['official-artwork']['front_default']
-        let types = getType(responseAsJson['types']); 
-        let jsonPokeData = {'name':name, 'sprite':sprite, 'types':types};
+        let types = getType(responseAsJson['types']);
+        let jsonPokeData = { 'name': name, 'sprite': sprite, 'types': types };
         pokemonData.push(jsonPokeData);
     }
     //console.log(responseAsJson);
 }
 
+async function showMore() {
+    await loadPokemon();
+    renderPokemonTiles(pokemonData,dataOffset);
+}
+
 function firstLetterToCapital(name) {
     let str = name;
     let modStr = str[0].toUpperCase() + str.slice(1);
-    
+
     return modStr;
 }
 
-function getType(data){
+function getType(data) {
     let types = [];
     for (let i = 0; i < data.length; i++) {
         const element = data[i];
