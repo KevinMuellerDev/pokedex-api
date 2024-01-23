8d3b3b738711d0d40c24e233ecf17e4d93ae12ce
Kevin Mueller <kevin.mueller@tuta.com>
2024-01-23 06:25:08 +0100

    refactored render.js; made own function to render the pokemon types for the tile

--- render.js
+++ render.js
@@ -1,18 +1,22 @@
 function renderPokemonTiles(data, dataOffs) {
-    for (let i = 0 + dataOffs ; i < data.length; i++) {
+    for (let i = 0 + dataOffs; i < data.length; i++) {
         const tile = data[i];
 
         document.getElementById('tiles-container').innerHTML += templateTile(i);
         document.getElementById(`tile${i}`).style.background = `var(--${tile.types[0]})`;
         document.getElementById(`pokemon-name${i}`).innerHTML = firstLetterToCapital(tile.name);
-        for (let j = 0; j < tile.types.length; j++) {
-            const element = tile.types[j];
-            console.log(element)
-            document.getElementById(`tile-info${i}`).innerHTML += templateType(element);
-        }
+        renderPokemonTileType(tile, i);
         document.getElementById(`sprite${i}`).src = tile.sprite
         document.getElementById(`sprite${i}`).alt = tile.name
     }
     dataOffset += 25;
+}
+
+function renderPokemonTileType(tile, index) {
+    for (let j = 0; j < tile.types.length; j++) {
+        const element = tile.types[j];
+        console.log(element)
+        document.getElementById(`tile-info${index}`).innerHTML += templateType(element);
+    }
 }
 
